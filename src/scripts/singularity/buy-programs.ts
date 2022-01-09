334d4905e952964105ec94d39328cadc9b9180e6
Chris Tristan <chris.tristan@protonmail.com>
2022-01-09 16:37:18 -0700

    Fixed buy programs script stopping other actions

--- src/scripts/singularity/buy-programs.ts
+++ src/scripts/singularity/buy-programs.ts
@@ -1,4 +1,4 @@
-import { NS } from "@ns"
+import { NS } from "@ns";
 
 /**
  * Buys any programs we can afford.
@@ -17,15 +17,11 @@ export async function main(ns: NS): Promise<void> {
         "AutoLink.exe",
         "ServerProfiler.exe",
         "Formulas.exe",
-    ]
+    ];
 
-    ns.purchaseTor()
+    ns.purchaseTor();
     for (let i = 0; i < programs.length; i++) {
-        const program = programs[i]
-        if (!ns.fileExists(program, "home") && ns.purchaseProgram(program)) {
-            // After purchasing a program, stop any action in case we're trying
-            // to create the program we just bought.
-            ns.stopAction()
-        }
+        const program = programs[i];
+        ns.purchaseProgram(program);
     }
 }
