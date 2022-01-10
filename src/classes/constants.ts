151f08cba0e9bea378c2fbb939f99e7139e334e0
Chris Tristan <chris.tristan@protonmail.com>
2022-01-09 19:55:47 -0700

    Updated work factions script to sort by lowest rep requirement and favor

--- src/classes/constants.ts
+++ src/classes/constants.ts
@@ -1,235 +1,237 @@
-import { ICities } from "/interfaces/icities.js";
-import { ICompanies } from "/interfaces/icompanies.js";
-import { ICrimes } from "/interfaces/icrimes.js";
-import { IFactions } from "/interfaces/ifactions.js";
-import { IPrograms } from "/interfaces/iprograms.js";
-import { IStats } from "/interfaces/istats.js";
-import { ITasks } from "/interfaces/itasks.js";
-import { IWorkTypes } from "/interfaces/iworktypes.js";
+// Bitburner-specific rule exceptions
+/* eslint-disable import/no-absolute-path */
+import { ICities } from '/interfaces/icities.js'
+import { ICompanies } from '/interfaces/icompanies.js'
+import { ICrimes } from '/interfaces/icrimes.js'
+import { IFactions } from '/interfaces/ifactions.js'
+import { IPrograms } from '/interfaces/iprograms.js'
+import { IStats } from '/interfaces/istats.js'
+import { ITasks } from '/interfaces/itasks.js'
+import { IWorkTypes } from '/interfaces/iworktypes.js'
 
 export const Factions: IFactions = {
-    // Main Quest
-    CyberSec: "CyberSec", // 18,750
-    NiteSec: "NiteSec", // 112,500
-    BlackHand: "The Black Hand", // 175,000
-    BitRunners: "BitRunners", // 1,000,000
-    Daedalus: "Daedalus", // 2,500,000
+  // Main Quest
+  CyberSec: 'CyberSec', // 18,750
+  NiteSec: 'NiteSec', // 112,500
+  BlackHand: 'The Black Hand', // 175,000
+  BitRunners: 'BitRunners', // 1,000,000
+  Daedalus: 'Daedalus', // 2,500,000
 
-    // Side Quests
-    Netburners: "Netburners", // 12,500
-    SlumSnakes: "Slum Snakes", // 22,500
-    Sector12: "Sector-12", // 50,000
-    Ishima: "Ishima", // 50,000
-    Volhaven: "Volhaven", // 50,000
-    Tetrads: "Tetrads", // 62,500
-    TianDiHui: "Tian Di Hui", // 75,000
-    Aevum: "Aevum", // 100,000
-    NewTokyo: "New Tokyo", // 112,500
-    Chongqing: "Chongqing", // 112,500
+  // Side Quests
+  Netburners: 'Netburners', // 12,500
+  SlumSnakes: 'Slum Snakes', // 22,500
+  Sector12: 'Sector-12', // 50,000
+  Ishima: 'Ishima', // 50,000
+  Volhaven: 'Volhaven', // 50,000
+  Tetrads: 'Tetrads', // 62,500
+  TianDiHui: 'Tian Di Hui', // 75,000
+  Aevum: 'Aevum', // 100,000
+  NewTokyo: 'New Tokyo', // 112,500
+  Chongqing: 'Chongqing', // 112,500
 
-    MegaCorp: "MegaCorp",
-    ECorp: "ECorp",
-    KuaiGong: "KuaiGong International",
-    FourSigma: "Four Sigma",
-    NWO: "NWO",
-    Blade: "Blade Industries",
-    OmniTek: "OmniTek Incorporated",
-    Bachman: "Bachman & Associates",
-    Clarke: "Clarke Incorporated",
-    Fulcrum: "Fulcrum Secret Technologies",
-    Silhouette: "Silhouette",
-    Speakers: "Speakers for the Dead",
-    DarkArmy: "The Dark Army",
-    Syndicate: "The Syndicate",
+  MegaCorp: 'MegaCorp',
+  ECorp: 'ECorp',
+  KuaiGong: 'KuaiGong International',
+  FourSigma: 'Four Sigma',
+  NWO: 'NWO',
+  Blade: 'Blade Industries',
+  OmniTek: 'OmniTek Incorporated',
+  Bachman: 'Bachman & Associates',
+  Clarke: 'Clarke Incorporated',
+  Fulcrum: 'Fulcrum Secret Technologies',
+  Silhouette: 'Silhouette',
+  Speakers: 'Speakers for the Dead',
+  DarkArmy: 'The Dark Army',
+  Syndicate: 'The Syndicate',
 
-    // Endgame
+  // Endgame
 
-    Covenant: "The Covenant",
-    Illuminati: "Illuminati",
-};
+  Covenant: 'The Covenant',
+  Illuminati: 'Illuminati'
+}
 
 export const Cities: ICities = {
-    // Starting city
-    Sector12: "Sector-12",
+  // Starting city
+  Sector12: 'Sector-12',
 
-    // Allied city
-    Aevum: "Aevum",
+  // Allied city
+  Aevum: 'Aevum',
 
-    // Other cities
-    Chongqing: "Chongqing",
-    Ishima: "Ishima",
-    NewTokyo: "New Tokyo",
-    Volhaven: "Volhaven",
-};
+  // Other cities
+  Chongqing: 'Chongqing',
+  Ishima: 'Ishima',
+  NewTokyo: 'New Tokyo',
+  Volhaven: 'Volhaven'
+}
 
 export const Companies: ICompanies = {
-    Fulcrum: { name: "Fulcrum Technologies", repReq: 250e3 },
-    KuaiGong: { name: "KuaiGong International", repReq: 200e3 },
-    Clarke: { name: "Clarke Incorporated", repReq: 200e3 },
-    FourSigma: { name: "Four Sigma", repReq: 200e3 },
-    Bachman: { name: "Bachman & Associates", repReq: 200e3 },
-    NWO: { name: "NWO", repReq: 200e3 },
-    Blade: { name: "Blade Industries", repReq: 200e3 },
-    ECorp: { name: "ECorp", repReq: 200e3 },
-    MegaCorp: { name: "MegaCorp", repReq: 200e3 },
-};
+  Fulcrum: { name: 'Fulcrum Technologies', repReq: 250e3 },
+  KuaiGong: { name: 'KuaiGong International', repReq: 200e3 },
+  Clarke: { name: 'Clarke Incorporated', repReq: 200e3 },
+  FourSigma: { name: 'Four Sigma', repReq: 200e3 },
+  Bachman: { name: 'Bachman & Associates', repReq: 200e3 },
+  NWO: { name: 'NWO', repReq: 200e3 },
+  Blade: { name: 'Blade Industries', repReq: 200e3 },
+  ECorp: { name: 'ECorp', repReq: 200e3 },
+  MegaCorp: { name: 'MegaCorp', repReq: 200e3 }
+}
 
 export const Crimes: ICrimes = {
-    Assassination: {
-        name: "assassination",
-        time: 300,
-        money: 12000000,
-        strExp: 300,
-        defExp: 300,
-        dexExp: 300,
-        agiExp: 300,
-        chaExp: 0,
-    },
-    BondForgery: {
-        name: "bond forgery",
-        time: 300,
-        money: 4500,
-        strExp: 0,
-        defExp: 0,
-        dexExp: 150,
-        agiExp: 0,
-        chaExp: 15,
-    },
-    DealDrugs: {
-        name: "deal drugs",
-        time: 10,
-        money: 120,
-        strExp: 0,
-        defExp: 0,
-        dexExp: 5,
-        agiExp: 5,
-        chaExp: 10,
-    },
-    GrandTheftAuto: {
-        name: "grand theft auto",
-        time: 80,
-        money: 1600,
-        strExp: 20,
-        defExp: 20,
-        dexExp: 20,
-        agiExp: 80,
-        chaExp: 40,
-    },
-    Heist: {
-        name: "heist",
-        time: 600,
-        money: 120000000,
-        strExp: 450,
-        defExp: 450,
-        dexExp: 450,
-        agiExp: 450,
-        chaExp: 450,
-    },
-    Homicide: {
-        name: "homicide",
-        time: 3,
-        money: 45,
-        strExp: 2,
-        defExp: 2,
-        dexExp: 2,
-        agiExp: 2,
-        chaExp: 0,
-    },
-    Larceny: {
-        name: "larceny",
-        time: 90,
-        money: 800,
-        strExp: 0,
-        defExp: 0,
-        dexExp: 60,
-        agiExp: 60,
-        chaExp: 0,
-    },
-    Kidnap: {
-        name: "kidnap",
-        time: 120,
-        money: 3600,
-        strExp: 80,
-        defExp: 80,
-        dexExp: 80,
-        agiExp: 80,
-        chaExp: 80,
-    },
-    Mug: {
-        name: "mug",
-        time: 4,
-        money: 36,
-        strExp: 3,
-        defExp: 3,
-        dexExp: 3,
-        agiExp: 3,
-        chaExp: 0,
-    },
-    RobStore: {
-        name: "rob store",
-        time: 60,
-        money: 400,
-        strExp: 0,
-        defExp: 0,
-        dexExp: 45,
-        agiExp: 45,
-        chaExp: 0,
-    },
-    Shoplift: {
-        name: "shoplift",
-        time: 2,
-        money: 15,
-        strExp: 0,
-        defExp: 0,
-        dexExp: 2,
-        agiExp: 2,
-        chaExp: 0,
-    },
-    TraffickArms: {
-        name: "traffick arms",
-        time: 40,
-        money: 600,
-        strExp: 20,
-        defExp: 20,
-        dexExp: 20,
-        agiExp: 20,
-        chaExp: 40,
-    },
-};
+  Assassination: {
+    name: 'assassination',
+    time: 300,
+    money: 12000000,
+    strExp: 300,
+    defExp: 300,
+    dexExp: 300,
+    agiExp: 300,
+    chaExp: 0
+  },
+  BondForgery: {
+    name: 'bond forgery',
+    time: 300,
+    money: 4500,
+    strExp: 0,
+    defExp: 0,
+    dexExp: 150,
+    agiExp: 0,
+    chaExp: 15
+  },
+  DealDrugs: {
+    name: 'deal drugs',
+    time: 10,
+    money: 120,
+    strExp: 0,
+    defExp: 0,
+    dexExp: 5,
+    agiExp: 5,
+    chaExp: 10
+  },
+  GrandTheftAuto: {
+    name: 'grand theft auto',
+    time: 80,
+    money: 1600,
+    strExp: 20,
+    defExp: 20,
+    dexExp: 20,
+    agiExp: 80,
+    chaExp: 40
+  },
+  Heist: {
+    name: 'heist',
+    time: 600,
+    money: 120000000,
+    strExp: 450,
+    defExp: 450,
+    dexExp: 450,
+    agiExp: 450,
+    chaExp: 450
+  },
+  Homicide: {
+    name: 'homicide',
+    time: 3,
+    money: 45,
+    strExp: 2,
+    defExp: 2,
+    dexExp: 2,
+    agiExp: 2,
+    chaExp: 0
+  },
+  Larceny: {
+    name: 'larceny',
+    time: 90,
+    money: 800,
+    strExp: 0,
+    defExp: 0,
+    dexExp: 60,
+    agiExp: 60,
+    chaExp: 0
+  },
+  Kidnap: {
+    name: 'kidnap',
+    time: 120,
+    money: 3600,
+    strExp: 80,
+    defExp: 80,
+    dexExp: 80,
+    agiExp: 80,
+    chaExp: 80
+  },
+  Mug: {
+    name: 'mug',
+    time: 4,
+    money: 36,
+    strExp: 3,
+    defExp: 3,
+    dexExp: 3,
+    agiExp: 3,
+    chaExp: 0
+  },
+  RobStore: {
+    name: 'rob store',
+    time: 60,
+    money: 400,
+    strExp: 0,
+    defExp: 0,
+    dexExp: 45,
+    agiExp: 45,
+    chaExp: 0
+  },
+  Shoplift: {
+    name: 'shoplift',
+    time: 2,
+    money: 15,
+    strExp: 0,
+    defExp: 0,
+    dexExp: 2,
+    agiExp: 2,
+    chaExp: 0
+  },
+  TraffickArms: {
+    name: 'traffick arms',
+    time: 40,
+    money: 600,
+    strExp: 20,
+    defExp: 20,
+    dexExp: 20,
+    agiExp: 20,
+    chaExp: 40
+  }
+}
 
 export const Programs: IPrograms = {
-    BruteSSH: { name: "BruteSSH.exe", hackLevelReq: 50 },
-    FTPCrack: { name: "FTPCrack.exe", hackLevelReq: 100 },
-    RelaySMTP: { name: "relaySMTP.exe", hackLevelReq: 250 },
-    HTTPWorm: { name: "HTTPWorm.exe", hackLevelReq: 500 },
-    SQLInject: { name: "SQLInject.exe", hackLevelReq: 750 },
-    DeepscanV1: { name: "DeepscanV1.exe", hackLevelReq: 75 },
-    DeepscanV2: { name: "DeepscanV2.exe", hackLevelReq: 400 },
-    AutoLink: { name: "AutoLink.exe", hackLevelReq: 25 },
-    ServerProfiler: { name: "ServerProfiler.exe", hackLevelReq: 75 },
-    Formulas: { name: "Formulas.exe", hackLevelReq: 1000 },
-};
+  BruteSSH: { name: 'BruteSSH.exe', hackLevelReq: 50 },
+  FTPCrack: { name: 'FTPCrack.exe', hackLevelReq: 100 },
+  RelaySMTP: { name: 'relaySMTP.exe', hackLevelReq: 250 },
+  HTTPWorm: { name: 'HTTPWorm.exe', hackLevelReq: 500 },
+  SQLInject: { name: 'SQLInject.exe', hackLevelReq: 750 },
+  DeepscanV1: { name: 'DeepscanV1.exe', hackLevelReq: 75 },
+  DeepscanV2: { name: 'DeepscanV2.exe', hackLevelReq: 400 },
+  AutoLink: { name: 'AutoLink.exe', hackLevelReq: 25 },
+  ServerProfiler: { name: 'ServerProfiler.exe', hackLevelReq: 75 },
+  Formulas: { name: 'Formulas.exe', hackLevelReq: 1000 }
+}
 
 export const Tasks: ITasks = {
-    Grow: "grow",
-    Hack: "hack",
-    Weaken: "weaken",
-};
+  Grow: 'grow',
+  Hack: 'hack',
+  Weaken: 'weaken'
+}
 
 export const Stats: IStats = {
-    Agility: "agility",
-    Charisma: "charisma",
-    Defense: "defense",
-    Dexterity: "dexterity",
-    Hacking: "hacking",
-    Strength: "strength",
-};
+  Agility: 'agility',
+  Charisma: 'charisma',
+  Defense: 'defense',
+  Dexterity: 'dexterity',
+  Hacking: 'hacking',
+  Strength: 'strength'
+}
 
 export const WorkTypes: IWorkTypes = {
-    Company: "Working for Company",
-    CompanyPartTime: "Working for Company part-time",
-    CreateProgram: "Working on Create a Program",
-    Crime: "Committing a crime",
-    Factions: "Working for Faction",
-    StudyClass: "Studying or Taking a class at university",
-};
+  Company: 'Working for Company',
+  CompanyPartTime: 'Working for Company part-time',
+  CreateProgram: 'Working on Create a Program',
+  Crime: 'Committing a crime',
+  Factions: 'Working for Faction',
+  StudyClass: 'Studying or Taking a class at university'
+}
--- src/scripts/singularity/work-factions.ts
+++ src/scripts/singularity/work-factions.ts
@@ -1,6 +1,8 @@
-import { NS } from "@ns"
-import * as Constants from "/classes/constants.js"
-import { isWorking } from "/scripts/utils.js"
+// Bitburner-specific rule exceptions
+/* eslint-disable import/no-absolute-path */
+import { NS } from '@ns'
+import * as Constants from '/classes/constants.js'
+import { isWorking } from '/scripts/utils.js'
 
 const Factions = Constants.Factions
 const workType = Constants.WorkTypes.Factions
@@ -11,51 +13,59 @@ const workType = Constants.WorkTypes.Factions
  *
  * @param {NS} ns
  */
-export async function main(ns: NS): Promise<void> {
-    const ownedAugs = ns.getOwnedAugmentations(true)
+export async function main (ns: NS): Promise<void> {
+  const ownedAugs = ns.getOwnedAugmentations(true)
 
-    for (const key in Factions) {
-        const faction = Factions[key]
+  let factions = []
+  for (const key in Factions) {
+    const faction = Factions[key]
 
-        // If we don't have any faction rep, that means we aren't a member yet
-        if (ns.getFactionRep(faction) > 0) {
-            // Only consider augmentations we don't own already
-            const augs = ns
-                .getAugmentationsFromFaction(faction)
-                .filter((aug) => ownedAugs.indexOf(aug) == -1)
-            let highestRepReq = 0
+    // We want to get the required rep based off of the augmentations that we
+    // don't own already
+    const augs = ns
+      .getAugmentationsFromFaction(faction)
+      .filter((aug) => !ownedAugs.includes(aug))
 
-            for (let j = 0; j < augs.length; j++) {
-                const repReq = ns.getAugmentationRepReq(augs[j])
-                highestRepReq = Math.max(highestRepReq, repReq)
-            }
-
-            // If we meet the criteria, let's work for this faction
-            if (ns.getFactionRep(faction) < highestRepReq) {
-                await workForFaction(ns, faction, highestRepReq)
-
-                // Keep working until we've either hit our desired rep or
-                // started other work.
-                while (
-                    ns.getFactionRep(faction) < highestRepReq &&
-                    isWorking(ns, workType)
-                ) {
-                    await workForFaction(ns, faction, highestRepReq)
-                }
-
-                // We're done working for this faction now
-                if (isWorking(ns, workType)) {
-                    ns.stopAction()
-                }
-                return
-            }
-        }
+    let highestRepReq = 0
+    for (let j = 0; j < augs.length; j++) {
+      const repReq = ns.getAugmentationRepReq(augs[j])
+      highestRepReq = Math.max(highestRepReq, repReq)
     }
 
-    // If we're still working on a faction, we can stop now
-    if (isWorking(ns, workType)) {
+    factions.push({
+      name: faction, favor: ns.getFactionFavor(faction), repReq: highestRepReq
+    })
+  }
+
+  // Sort by least favor so we can improve our most needed factions first
+  // But sort by lowest reputation requirement first in the case of ties
+  factions = factions.sort((a, b) => a.repReq - b.repReq)
+  factions = factions.sort((a, b) => a.favor - b.favor)
+
+  for (const faction of factions) {
+    // If we don't have any faction rep, that means we aren't a member yet
+    if (ns.getFactionRep(faction.name) > 0 &&
+      ns.getFactionRep(faction.name) < faction.repReq) {
+      do {
+        // Keep working until we've either hit our desired rep or
+        // started other work.
+        await workForFaction(ns, faction.name, faction.repReq)
+      } while (ns.getFactionRep(faction.name) < faction.repReq &&
+        isWorking(ns, workType))
+
+      // We're done working for this faction now
+      if (isWorking(ns, workType)) {
         ns.stopAction()
+      }
+
+      ns.exit()
     }
+  }
+
+  // If we're still working on a faction, we can stop now
+  if (isWorking(ns, workType)) {
+    ns.stopAction()
+  }
 }
 
 /**
@@ -64,56 +74,56 @@ export async function main(ns: NS): Promise<void> {
  *
  * @param {NS} ns
  */
-async function workForFaction(
-    ns: NS,
-    faction: string,
-    repReq: number
+async function workForFaction (
+  ns: NS,
+  faction: string,
+  repReq: number
 ): Promise<void> {
-    let rep = ns.getFactionRep(faction)
+  let rep = ns.getFactionRep(faction)
 
-    // First try donating money to increase rep the fastest
-    rep = donateToFaction(ns, faction, repReq)
+  // First try donating money to increase rep the fastest
+  rep = donateToFaction(ns, faction, repReq)
 
-    if (rep < repReq) {
-        const focus = ns.isFocused()
+  if (rep < repReq) {
+    const focus = ns.isFocused()
 
-        if (
-            !ns.workForFaction(faction, "Hacking", focus) &&
-            !ns.workForFaction(faction, "Field Work", focus)
-        ) {
-            ns.workForFaction(faction, "Security Work", focus)
-        }
-
-        // Sleep until we gain 1000 rep or start working on something else
-        while (ns.getPlayer().workRepGained < 1000 && isWorking(ns, workType)) {
-            await ns.sleep(1000)
-        }
+    if (
+      !ns.workForFaction(faction, 'Hacking', focus) &&
+            !ns.workForFaction(faction, 'Field Work', focus)
+    ) {
+      ns.workForFaction(faction, 'Security Work', focus)
     }
+
+    // Sleep until we gain 1000 rep or start working on something else
+    while (ns.getPlayer().workRepGained < 1000 && isWorking(ns, workType)) {
+      await ns.sleep(1000)
+    }
+  }
 }
 
 /**
  * @param {NS} ns
  */
-function donateToFaction(ns: NS, faction: string, repReq: number): number {
-    const favor = ns.getFactionFavor(faction)
-    let rep = ns.getFactionRep(faction)
+function donateToFaction (ns: NS, faction: string, repReq: number): number {
+  const favor = ns.getFactionFavor(faction)
+  let rep = ns.getFactionRep(faction)
 
-    if (favor >= 150) {
-        const repDifference = repReq - rep
-        const repMultiplier = favor / 100
+  if (favor >= 150) {
+    const repDifference = repReq - rep
+    const repMultiplier = favor / 100
 
-        /**
+    /**
          * Formula for reputation gain:
          * reputation = donationAmount * reputationMultiplier / 10e6
          *
          * Formula for donation amount:
          * donationAmount = reputation * 10e6 / reputationMultiplier
          */
-        const donationAmount = Math.ceil((repDifference * 10e6) / repMultiplier)
-        ns.donateToFaction(faction, donationAmount)
+    const donationAmount = Math.ceil((repDifference * 10e6) / repMultiplier)
+    ns.donateToFaction(faction, donationAmount)
 
-        rep = ns.getFactionRep(faction)
-    }
+    rep = ns.getFactionRep(faction)
+  }
 
-    return rep
+  return rep
 }
