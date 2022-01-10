3b5d4b2184e8d8b6bc3bb98526f898a68b7f65c5
Chris Tristan <chris.tristan@protonmail.com>
2022-01-09 19:31:51 -0700

    Updated create programs script to stop when it already exists but we are still creating it

--- src/scripts/singularity/create-programs.ts
+++ src/scripts/singularity/create-programs.ts
@@ -1,39 +1,48 @@
-import { NS } from "@ns";
-import * as Constants from "/classes/constants.js";
-import { isWorking } from "/scripts/utils.js";
+// Bitburner-specific rule exceptions
+/* eslint-disable import/no-absolute-path */
+import { NS } from '@ns'
+import * as Constants from '/classes/constants.js'
+import { isWorking } from '/scripts/utils.js'
 
-const Programs = Constants.Programs;
-const workType = Constants.WorkTypes.CreateProgram;
+const workType = Constants.WorkTypes.CreateProgram
 
 /** @param {NS} ns **/
-export async function main(ns: NS): Promise<void> {
-    let programs = [];
-    for (const key in Programs) {
-        const program = Programs[key];
-        programs.push({
-            name: program.name,
-            hackLevelReq: program.hackLevelReq,
-        });
-    }
+export async function main (ns: NS): Promise<void> {
+  let programs = []
+  for (const key in Constants.Programs) {
+    const program = Constants.Programs[key]
 
-    // Sort the programs by hacking level required so we get the easy
-    // ones done first
-    programs = programs.sort((a, b) => a.hackLevelReq - b.hackLevelReq);
+    programs.push({
+      name: program.name,
+      hackLevelReq: program.hackLevelReq
+    })
+  }
 
-    for (const program of programs)
-        if (
-            !ns.fileExists(program.name) &&
+  // Sort the programs by hacking level required so we get the easy
+  // ones done first
+  programs = programs.sort((a, b) => a.hackLevelReq - b.hackLevelReq)
+
+  for (const program of programs) {
+    if (
+      !ns.fileExists(program.name) &&
             ns.getHackingLevel() >= program.hackLevelReq &&
             ns.createProgram(program.name)
-        ) {
-            // Keep checking that we haven't bought the program since starting
-            // to create the program
-            while (!ns.fileExists(program.name) && isWorking(ns, workType)) {
-                await ns.sleep(1000);
-            }
+    ) {
+      // Keep checking that we haven't bought the program since starting to
+      // create the program
+      while (!ns.fileExists(program.name) && isWorking(ns, workType)) {
+        await ns.sleep(1000)
+      }
 
-            // We're done writing the program, let's exit so we don't
-            // interrupt anything else
-            ns.exit();
-        }
+      // We're done writing the program, let's exit so we don't
+      // interrupt anything else
+      ns.exit()
+    }
+  }
+
+  // If we have no programs left to create but are still working on one,
+  // let's stop that
+  if (isWorking(ns, workType)) {
+    ns.stopAction()
+  }
 }
