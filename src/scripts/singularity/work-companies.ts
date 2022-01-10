906eef9c164457b686f6450ccdc0dee627c236bc
Chris Tristan <chris.tristan@protonmail.com>
2022-01-10 06:40:56 -0700

    Fix an infinite loop

--- src/scripts/singularity/work-companies.ts
+++ src/scripts/singularity/work-companies.ts
@@ -1,48 +1,49 @@
-import { NS } from "@ns";
-import * as Constants from "/classes/constants.js";
-import { isWorking } from "/scripts/utils.js";
+// Bitburner-specific rule exceptions
+/* eslint-disable import/no-absolute-path */
+import { NS } from '@ns'
+import * as Constants from '/classes/constants.js'
+import { isWorking } from '/scripts/utils.js'
 
-const Companies = Constants.Companies;
+const Companies = Constants.Companies
 
-const workType = Constants.WorkTypes.Company;
+const workType = Constants.WorkTypes.Company
 
 /**
  * Works for the most profitable company we qualify for.
  *
  * @param {NS} ns
  */
-export async function main(ns: NS): Promise<void> {
-    for (const key in Companies) {
-        const company = Companies[key];
+export async function main (ns: NS): Promise<void> {
+  for (const key in Companies) {
+    const company = Companies[key]
 
-        ns.applyToCompany(company.name, "Software");
+    ns.applyToCompany(company.name, 'Software')
 
-        if (
-            ns.getCompanyRep(company.name) < company.repReq &&
-            ns.workForCompany(company.name, ns.isFocused())
+    if (
+      ns.getCompanyRep(company.name) < company.repReq &&
+      ns.workForCompany(company.name, ns.isFocused())
+    ) {
+      // Work until we meet the reputation requirement or start working
+      // on something else
+      while (
+        ns.getCompanyRep(company.name) < company.repReq &&
+        isWorking(ns, workType)
+      ) {
+        ns.applyToCompany(company.name, 'Software')
+        ns.workForCompany(company.name, ns.isFocused())
+
+        // Sleep until we gain 1000 rep (taking the early cancel penalty
+        // into account) or start working on something else
+        while (
+          ns.getPlayer().workRepGained < 1000 * 2 &&
+          isWorking(ns, workType)
         ) {
-            // Work until we meet the reputation requirement or start working
-            // on something else
-            while (
-                ns.getCompanyRep(company.name) < company.repReq &&
-                isWorking(ns, workType)
-            ) {
-                if (ns.applyToCompany(company.name, "Software")) {
-                    ns.workForCompany(company.name, ns.isFocused());
-                }
-
-                // Sleep until we gain 1000 rep (taking the early cancel penalty
-                // into account) or start working on something else
-                while (
-                    ns.getPlayer().workRepGained < 1000 * 2 &&
-                    isWorking(ns, workType)
-                ) {
-                    await ns.sleep(1000);
-                }
-            }
-
-            // We got our rep so let's exit so we don't interrupt anything
-            ns.exit();
+          await ns.sleep(1000)
         }
+      }
+
+      // We got our rep so let's exit so we don't interrupt anything
+      ns.exit()
     }
+  }
 }
