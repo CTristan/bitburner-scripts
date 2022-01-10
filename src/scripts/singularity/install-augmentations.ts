f2d22ecca03d43312e2d716fa38265ee9572878b
Chris Tristan <chris.tristan@protonmail.com>
2022-01-09 21:07:06 -0700

    Updated install augs script to install if we purchased all of a factions augs

--- src/scripts/singularity/install-augmentations.ts
+++ src/scripts/singularity/install-augmentations.ts
@@ -1,6 +1,8 @@
-import { NS } from "@ns"
-import * as Constants from "/classes/constants.js"
-import { forceRunScript } from "/scripts/utils.js"
+// Bitburner-specific rule exceptions
+/* eslint-disable import/no-absolute-path */
+import { NS } from '@ns'
+import * as Constants from '/classes/constants.js'
+import { forceRunScript } from '/scripts/utils.js'
 
 const Factions = Constants.Factions
 
@@ -10,35 +12,73 @@ const Factions = Constants.Factions
  *
  * @param {NS} ns
  */
-export async function main(ns: NS): Promise<void> {
-    const doInstall = false
+export async function main (ns: NS): Promise<void> {
+  await checkFactionFavor(ns)
+  await checkPurchasedFactionAugs(ns)
+}
 
-    // Check if installing would push a faction's favor past
-    // the donation threshold
-    for (const key in Factions) {
-        const faction = Factions[key]
-        const currentFavor = ns.getFactionFavor(faction)
+/**
+ * Checks if installing would push a faction's favor past
+ * the donation threshold.
+ *
+ * @param {NS} ns
+ */
+async function checkFactionFavor (ns: NS): Promise<void> {
+  for (const key in Factions) {
+    const faction = Factions[key]
+    const currentFavor = ns.getFactionFavor(faction)
 
-        if (currentFavor < 150) {
-            const favorGain = ns.getFactionFavorGain(faction)
+    if (currentFavor < 150) {
+      const favorGain = ns.getFactionFavorGain(faction)
 
-            if (currentFavor + favorGain >= 150) {
-                await installAugmentations(ns)
-            }
-        }
-    }
-
-    if (doInstall) {
+      if (currentFavor + favorGain >= 150) {
         await installAugmentations(ns)
+      }
     }
+  }
+}
+
+/**
+ * Checks if we have purchased every augmentation a faction sells but don't have
+ * them installed yet.
+ *
+ * @param ns
+ */
+async function checkPurchasedFactionAugs (ns: NS): Promise<void> {
+  const ownedAugs = ns.getOwnedAugmentations(true)
+  const installedAugs = ns.getOwnedAugmentations()
+  const purchasedAugs = ownedAugs.filter(aug => !installedAugs.includes(aug))
+
+  for (const key in Factions) {
+    const faction = Factions[key]
+    const factionAugs = ns.getAugmentationsFromFaction(faction)
+    const remainingFactionAugs = factionAugs.filter(
+      aug => !ownedAugs.includes(aug)
+    )
+
+    /**
+     * Check if there are no more remaining augmentations and there is at
+     * least one purchased aug from the faction. The assumption is that in this
+     * situation we just finished installing all of the faction's augmentations.
+     */
+    if (remainingFactionAugs.length === 0) {
+      for (const purchasedAug of purchasedAugs) {
+        if (factionAugs.includes(purchasedAug)) {
+          // If we have a purchased augmentation from the faction and they have
+          // no more remaining then we want to install them
+          await installAugmentations(ns)
+        }
+      }
+    }
+  }
 }
 
 /**
  * @param {NS} ns
  */
-export async function installAugmentations(ns: NS): Promise<void> {
-    // Before installing augmentations, try to make any last-minute purchases
-    await forceRunScript(ns, "/scripts/purchase-any-augmentations.js")
+async function installAugmentations (ns: NS): Promise<void> {
+  // Before installing augmentations, try to make any last-minute purchases
+  await forceRunScript(ns, '/scripts/purchase-any-augmentations.js')
 
-    ns.installAugmentations("first-run.js")
+  ns.installAugmentations('first-run.js')
 }
