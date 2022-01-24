d06a7d04eb50e5c0d9dbc659a2324d5d6d75c4d2
Chris Tristan <chris.tristan@protonmail.com>
2022-01-24 04:01:54 -0700

    Fixed install augs script to purchase all augs in a function rather than a script that doesn't have time to run before installing.

--- src/scripts/singularity/install-augmentations.ts
+++ src/scripts/singularity/install-augmentations.ts
@@ -134,9 +134,9 @@ function getPurchasedAugs(ns: NS): string[] {
 /**
  * @param {NS} ns
  */
-async function installAugmentations(ns: NS): Promise<void> {
+function installAugmentations(ns: NS): void {
     // Before installing augmentations, try to make any last-minute purchases
-    await forceRunScript(ns, "/scripts/purchase-any-augmentations.js")
+    purchaseAllAugmentations(ns)
 
     // Final check that we actually have any augmentations to install after
     // trying to purchase any available ones
@@ -146,5 +146,70 @@ async function installAugmentations(ns: NS): Promise<void> {
         ns.stopAction()
 
         ns.installAugmentations("first-run.js")
+    }
+}
+
+/**
+ * Purchase any augmentations we can afford.
+ * Should only run before installing augmentations.
+ *
+ * @param ns ns
+ */
+function purchaseAllAugmentations(ns: NS): void {
+    let ownedAugs = ns.getOwnedAugmentations(true)
+    for (const key in Factions) {
+        const faction = Factions[key]
+        const factionAugs = ns.getAugmentationsFromFaction(faction.name)
+
+        // Build a list of augs for the faction that we don't already own.
+        let augs = []
+        for (let j = 0; j < factionAugs.length; j++) {
+            const augName = factionAugs[j]
+
+            // If we don't already own the aug, let's add it to our list
+            if (!ownedAugs.includes(augName)) {
+                const augCost = ns.getAugmentationPrice(augName)
+                augs.push({ cost: augCost, name: augName })
+            }
+        }
+
+        // Sort the list of augs by cost descending so we can try to purchase the most expensive ones first
+        augs = augs.sort((a, b) => b.cost - a.cost)
+
+        /**
+         * If there aren't any new augmentations available, continue to
+         * the next faction
+         */
+        if (augs.length === 0) {
+            continue
+        }
+
+        for (let j = 0; j < augs.length; j++) {
+            const augName = augs[j].name
+
+            /**
+             * Try to buy the most expensive augmentation available from the faction.
+             * If we can, then add it to our list of owned augs and try to buy the next most expensive one.
+             * Otherwise, move on to the next faction.
+             *
+             * This way, we only ever attempt to purchase the most expensive aug from each faction and
+             * not just end up buying the cheapest ones which increases the cost multiplier.
+             */
+            if (ns.purchaseAugmentation(faction.name, augName)) {
+                ownedAugs = ns.getOwnedAugmentations(true)
+            }
+        }
+    }
+
+    /**
+     * Now that we bought all of the faction-specific augmentations, let's spend
+     * the rest of our money on the generic one
+     */
+    const alwaysAvailableAug = Constants.AlwaysAvailableAugmentation
+    for (const key in Factions) {
+        const faction = Factions[key]
+        while (ns.purchaseAugmentation(faction.name, alwaysAvailableAug)) {
+            // Purchase successful, do it again
+        }
     }
 }
